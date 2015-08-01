46d4e774158fceb490fd1f8cb6a7a426ac5ffc81
unknown-user <unknown-user@192.168.0.57>
2015-08-01 13:04:57 +0530

    Initial check-in of module AndroidGesture

---
+++ GestureListener.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+namespace AndroidGesture
+{
+	class GestureListener: Java.Lang.Object, GestureDetector.IOnGestureListener
+	{
+		public event Action LeftEvent;
+		public event Action RightEvent;
+		private static int SWIPE_MAX_OFF_PATH = 250;
+		private static int SWIPE_MIN_DISTANCE = 100;
+		private static int SWIPE_THRESHOLD_VELOCITY = 200;
+
+		public GestureListener()
+		{
+		}
+
+		public bool OnFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+		{
+			try
+			{
+				if ( Math.Abs ( e1.GetY () - e2.GetY () ) > SWIPE_MAX_OFF_PATH )
+					return false;
+				// right to left swipe
+				if ( e1.GetX () - e2.GetX () > SWIPE_MIN_DISTANCE && Math.Abs ( velocityX ) > SWIPE_THRESHOLD_VELOCITY && LeftEvent != null )
+				{
+					
+					LeftEvent ();//Toast.MakeText(view.Context, "Left Swipe", ToastLength.Short).Show();G
+				
+				}
+				else if ( e2.GetX () - e1.GetX () > SWIPE_MIN_DISTANCE && Math.Abs ( velocityX ) > SWIPE_THRESHOLD_VELOCITY && RightEvent != null )
+				{
+					if(e1.GetX()<100)
+					{
+					Console.WriteLine("e1.GetX() : "+	e1.GetX());
+					Console.WriteLine("e2.GetX() : "+	e2.GetX()); 
+					RightEvent ();// Toast.MakeText(view.Context, "Right Swipe", ToastLength.Short).Show(); 
+					}
+				}
+			}
+			catch ( Exception e )
+			{
+				// nothing
+				Console.WriteLine ( "Gesture listener didn't worked properly..." +e.Message);
+			}
+			return false;
+		}
+
+		public bool OnDown(MotionEvent e) 
+		{ 
+			return true;
+		}
+		public void OnLongPress(MotionEvent e) {}
+		public bool OnScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+		{ 
+			return true; 
+		}
+		public void OnShowPress(MotionEvent e) {}
+		public bool OnSingleTapUp(MotionEvent e) 
+		{ 
+			return true;
+		}	
+	}
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidGesture
+{
+	[Activity ( Label = "AndroidGesture" , MainLauncher = true , Icon = "@drawable/icon" )]
+	public class MainActivity : Activity
+	{
+		//http://forums.xamarin.com/discussion/comment/40052/
+		GestureDetector _gestureDetector;
+		GestureListener _gestureListener;
+		protected override void OnCreate ( Bundle bundle )
+		{
+			base.OnCreate ( bundle );
+ 
+			SetContentView ( Resource.Layout.Main );
+
+			_gestureListener = new GestureListener (); 
+
+			_gestureListener.LeftEvent += GestureLeft;
+			_gestureListener.RightEvent += GestureRight;
+ 
+			_gestureDetector = new GestureDetector (this,_gestureListener);
+
+
+		}
+		void GestureLeft()
+		{
+			Toast.MakeText (this, "Gesture Left", ToastLength.Short).Show ();
+		}
+
+		void GestureRight()
+		{
+			Toast.MakeText (this, "Gesture Right", ToastLength.Short).Show ();
+		}
+
+		public override bool DispatchTouchEvent (MotionEvent ev)
+		{
+			_gestureDetector.OnTouchEvent ( ev );
+			return base.DispatchTouchEvent (ev);
+
+
+		}
+	}
+}
+
+
